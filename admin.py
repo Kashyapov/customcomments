ae8e2f27a378cc0124654b88827da192ab4db39f
Vladyslav T <vladka@webdevelop.pro>
2015-11-20 19:31:30 +0200

    update admin view

--- admin.py
+++ admin.py
@@ -1,17 +1,26 @@
-# from django.contrib.comments import Comment
-# from django.contrib.comments.moderation import CommentModerator, moderator
-# from cms.models import Page
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _, ungettext
+
+from django_comments.admin import CommentsAdmin
 from customcomments import MyComment
 
 
-# class PageModerator(CommentModerator):
-#     email_notification = True
-#
-#     def moderate(self, comment, content_object, request):
-#         return True
-#
-# moderator.register(Page, PageModerator)
+class MyCommentsAdmin(CommentsAdmin):
+  fieldsets = (
+      (
+          None,
+          {'fields': ('content_type', 'object_pk', 'site')}
+      ),
+      (
+          _('Content'),
+          {'fields': ('user', 'user_name', 'image', 'user_email', 'user_url', 'comment')}
+      ),
+      (
+          _('Metadata'),
+          {'fields': ('submit_date', 'ip_address', 'is_public', 'is_removed')}
+      ),
+  )
 
-admin.site.register(MyComment)
+  list_display = ('name', 'image', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'is_public', 'is_removed')
 
+admin.site.register(MyComment, MyCommentsAdmin)
